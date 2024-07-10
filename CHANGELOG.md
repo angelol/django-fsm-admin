46baf4a22e319200aaf83228dc956f08a5bc09f0
Sami El Achi <mohamadashi7@gmail.com>
2024-07-10 13:58:21 +0400

    Update repo settings and config

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## [2.0.0] - 2024-07-10
+- Replaced django-fsm with django-fsm-2
+- Updated example project to work with Django 5.0
+
+- Dropped support for Django < 4.2
+- Added support for Django 4.2, 5.0 & 5.1
+- Dropped support for Python < 3.8
+- Added support for Python 3.8, 3.9, 3.10 & 3.11
--- setup.py
+++ setup.py
@@ -1,42 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-import sys
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 import fsm_admin
 
 readme = open("README.rst").read()
 
 setup(
-    name="django-fsm-admin",
+    name="django-fsm-admin-2",
     version=fsm_admin.__version__,
     author=fsm_admin.__author__,
-    description="Integrate django-fsm state transitions into the django admin",
+    description="Integrate django-fsm-2 state transitions into the django admin",
     long_description=readme,
     long_description_content_type="text/x-rst",
-    author_email="software@gadventures.com",
-    url="https://github.com/gadventures/django-fsm-admin",
+    author_email="dev@coral.li",
+    url="https://github.com/coral-li/django-fsm-admin-2",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "Django>=1.6",
+        "Django>=4.2",
         "django-fsm-2>=3.0.0",
     ],
     keywords="django fsm admin",
     license="MIT",
     platforms=["any"],
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
-        "Framework :: Django",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.0",
+        "Framework :: Django :: 5.1",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-    ]
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
